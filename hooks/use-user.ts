9d4c3a0307f6db9f30b00cd40aed41c61fbc5a13
Simon Weniger <simonweniger.sw@gmail.com>
2025-10-01 21:18:06 +0200

    cleanup

--- hooks/use-user.ts
+++
@@ -1,6 +0,0 @@
-import { useUser as useClerkUser } from '@clerk/nextjs';
-
-export const useUser = () => {
-  const { user } = useClerkUser();
-  return user;
-};
--- lib/error/handle.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from 'sonner';
-import { parseError } from './parse';
-
-export const handleError = (title: string, error: unknown) => {
-  const description = parseError(error);
-
-  toast.error(title, { description });
-};
